5653698e75478282dfe15b4da51b433b22ac94af
kpalf <kpalf@palfreyman.org>
2017-02-20 22:53:45 +0000

    Move accidental tagging command to separate script

---
+++ apamacore-centos_amd64/tag.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker tag kpalf/apamacore:centos_amd64 kpalf/apamacore:9.12.0.3_centos_amd64
--- apamacore-centos_amd64/test-version.sh
+++ apamacore-centos_amd64/test-version.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 docker run --rm kpalf/apamacore:centos_amd64 correlator --version
-docker tag kpalf/apamacore:centos_amd64 kpalf/apamacore:9.12.0.3_centos_amd64
