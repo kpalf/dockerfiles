0cfad11b386fb4ed076faed0efd420e0358b0584
Kev Palfreyman <kev_palfreyman@hotmail.com>
2018-03-24 12:40:04 +0000

    Update to Apama Core Community Edition 10.1.0.5

--- apamacore-centos_amd64/tag.sh
+++ apamacore-centos_amd64/tag.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker tag apamacore:centos_amd64 kpalf/apamacore:10.1.0.3_centos_amd64
-docker tag apamacore:centos_amd64 kpalf/apamacore:10.1.0.3
+docker tag apamacore:centos_amd64 kpalf/apamacore:10.1.0.5_centos_amd64
+docker tag apamacore:centos_amd64 kpalf/apamacore:10.1.0.5
 docker tag apamacore:centos_amd64 kpalf/apamacore:centos_amd64
 docker tag apamacore:centos_amd64 kpalf/apamacore:latest
