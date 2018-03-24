e0c75159823b525857ea093ae6b2502160361f42
Kev Palfreyman <kev_palfreyman@hotmail.com>
2018-03-24 18:20:36 +0000

    Update to Apama Core Community Edition 10.1.0.5

--- apama_core_nanoserver_amd64/test-version.bat
+++ apama_core_nanoserver_amd64/test-version.bat
@@ -1,2 +1,2 @@
 docker run --rm apamacore:nanoserver_amd64 correlator --version
-docker run --rm kpalf/apamacore:10.1.0.3_nanoserver_amd64_1709 correlator --version
+docker run --rm kpalf/apamacore:10.1.0.5_nanoserver_amd64_1709 correlator --version
