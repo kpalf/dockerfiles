1973d8cc4d8d183fe1741b88b4dcc469207ef031
Kev Palfreyman <kev_palfreyman@hotmail.com>
2018-05-12 13:39:25 +0100

    Initial checkin of Apama Core Community Edition version 10.2.0.1 on an Ubuntu 18.04 linux base image.  The Ubuntu 18.04 base image is smaller than the CentOS 7.4 base image, but requires a Multistage build.  Also change base install path to /opt/softwareag to be more similar to official images.

---
+++ apamacore-ubuntu_amd64/tag.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker tag apamacore:ubuntu_amd64 kpalf/apamacore:10.2.0.1_ubuntu_amd64
+docker tag apamacore:ubuntu_amd64 kpalf/apamacore:ubuntu_amd64
---
+++ apamacore-ubuntu_amd64/test-version.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run --rm kpalf/apamacore:10.2.0.1_ubuntu_amd64 correlator --version
