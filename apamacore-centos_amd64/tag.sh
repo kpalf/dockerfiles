069d6186713ac5e72c269186fb0e0ebeaf291278
Kev Palfreyman <kev_palfreyman@hotmail.com>
2018-10-14 23:54:30 +0100

    Fix typo in versions in apamacore-centos_amd64/tag.sh

--- apamacore-centos_amd64/tag.sh
+++ apamacore-centos_amd64/tag.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker tag apamacore:centos_amd64 kpalf/apamacore:10.3.0.1_centos_amd64
-docker tag apamacore:centos_amd64 kpalf/apamacore:10.1.0.1
+docker tag apamacore:centos_amd64 kpalf/apamacore:10.3.0.1
 docker tag apamacore:centos_amd64 kpalf/apamacore:centos_amd64
 docker tag apamacore:centos_amd64 kpalf/apamacore:latest
