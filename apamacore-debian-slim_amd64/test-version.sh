a6e69e997dea0b0c3df3662cc263705694ed02b6
Kev Palfreyman <kev_palfreyman@hotmail.com>
2019-04-28 15:46:03 +0100

    [DEBIAN-SLIM] Update to Apama Core Community Edition 10.3.1.1

--- apamacore-debian-slim_amd64/test-version.sh
+++ apamacore-debian-slim_amd64/test-version.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm kpalf/apamacore:10.3.0.1_experimental-debian-slim_amd64 correlator --version
+docker run --rm kpalf/apamacore:10.3.1.1_experimental-debian-slim_amd64 correlator --version
