2888e463753ca9f8ddb836bd4c659208916765c2
Kev Palfreyman <kev_palfreyman@hotmail.com>
2019-04-28 18:07:53 +0100

    [ARMv7HF RASPBIAN JESSIE] Update to Apama Core Community Edition 10.3.1.1

--- apamacore-raspbian_armv7hf/test-version.sh
+++ apamacore-raspbian_armv7hf/test-version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker run --rm apamacore:raspbian_armv7hf correlator --version
-docker run --rm kpalf/apamacore:10.3.0.1_raspbian_armv7hf correlator --version
+docker run --rm kpalf/apamacore:10.3.1.1_raspbian_armv7hf correlator --version
 docker run --rm kpalf/apamacore:raspbian_armv7hf correlator --version
